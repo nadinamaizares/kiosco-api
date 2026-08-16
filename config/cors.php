e1baaf4152ed23fae097548c746fb681ec69d29b
Nadina Belen Maizares <nadina.maizares@educacion.local>
2026-08-15 23:14:44 -0300

    chore: permitir CORS desde el frontend de Netlify

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'https://kiosco-ac.netlify.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
